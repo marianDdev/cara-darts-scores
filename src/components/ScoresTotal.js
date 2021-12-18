4014d35558ed22ccf9fcc0f32f1603ac5dd25340
Marian Dumitru <d.mariandev@gmail.com>
2021-12-18 21:19:05 +0200

    text correction

--- src/components/ScoresTotal.js
+++ src/components/ScoresTotal.js
@@ -5,7 +5,7 @@ import classes from './Score.module.css';
 const ScoresTotal = (props) => {
   return (
     <li className={classes.score}>
-      <h2>Your sum of scores</h2>
+      <h2>Your total of scores</h2>
       <h2>{props.title}</h2>
     </li>
   );
