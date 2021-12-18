3c29e5d34e3033a610f8026b172a0b315f6b381e
Marian Dumitru <d.mariandev@gmail.com>
2021-12-18 21:32:12 +0200

    firebase

---
+++ build/precache-manifest.c1497dc5814b8db3de8710d111cd7765.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78225aefd3796be4694af34a5a0e1169",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8799f19680df14de1b9b",
+    "url": "/static/css/main.97f64483.chunk.css"
+  },
+  {
+    "revision": "5465673c7c58d97955b5",
+    "url": "/static/js/2.6562a3f6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.6562a3f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8799f19680df14de1b9b",
+    "url": "/static/js/main.5cefad94.chunk.js"
+  },
+  {
+    "revision": "f13d5fb20258b992088c",
+    "url": "/static/js/runtime-main.a6b2f021.js"
+  }
+]);
